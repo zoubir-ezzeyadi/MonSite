ed8bf65112e67a60e9bd17a634cb73c382041657
ZZ <zoubir.ezzeyadi@yahoo.fr>
2020-01-06 16:02:51 +0100

    Première version du site

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<h1 class="display-4 text-center text-info">Découverte GIT/GITHUB</h1><hr>
+    
+</body>
+</html>
